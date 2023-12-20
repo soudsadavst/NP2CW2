576cfb690e94a3101803b8e65e820d404b2a5b4b
soudsadavst <ptsvsss6@gmail.com>
2023-12-20 20:43:59 +0700

    first commit

---
+++ lib/drawerleft.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:learn3/listmenu.dart';
+
+class drawerleft extends StatelessWidget {
+  const drawerleft({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          UserAccountsDrawerHeader(
+            currentAccountPicture: Icon(
+              Icons.face,
+              size: 48.0,
+              color: Colors.white,
+            ),
+            accountName: Text('Soudsada'),
+            accountEmail: Text('Soudsada@gmail.com'),
+            otherAccountsPictures: [
+              Icon(
+                Icons.bookmark_border,
+                color: Colors.white,
+              )
+            ],
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/cartoon.jp g'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          listmenu(),
+        ],
+      ),
+    );
+  }
+}
